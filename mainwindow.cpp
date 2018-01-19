6eee91690696661ae11b8a203aacf4342d3ac755
Vitaliy Murashov <vmurashov85@gmail.com>
2018-01-19 15:49:57 +0300

    initial commit

---
+++ mainwindow.cpp
@@ -0,0 +1,30 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+float uSrc; // Напряжение источника питания
+int iLed; // Ток диода
+float uLed; // Прямое напряжение диода
+float iCalcRes;
+float iAbsRes;
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+
+void MainWindow::on_btnCalc_clicked()
+{
+    uSrc = (ui->uSrcBox->toPlainText()).toFloat();
+    uLed = (ui->uLedBox->toPlainText()).toFloat();
+    iLed = (ui->iLedBox->toPlainText()).toInt();
+    QString qOut;
+    ui->wResWattBox->setText(qOut.setNum(uSrc));
+}
